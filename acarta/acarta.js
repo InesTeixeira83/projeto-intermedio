080fb9f2fe1847f3e061319d881bfd38265cbdb0
Inês Teixeira <shark12030@sharkcoders.pt>
2026-06-26 18:54:20 +0100

    versão de apresentação

--- acarta/acarta.js
+++ acarta/acarta.js
@@ -1,189 +1,243 @@
+import globals from './globals.js'
+
 const loading = document.getElementById("loading")
-const mycanvas = document.getElementById("mycanvas")
-const ctx = mycanvas.getContext("2d")
-const floor = 440
-const passos = 10
+const progress = document.getElementById("progress")
+const mycanvas = globals.canvas
+const ctx = globals.ctx
+const step_distance = globals.step_distance
+const floor = globals.floor
 
 const stage = {
     scene: null,
-    actors: [
-
-    ]
+    actors: [],
 }
-
 const scenes = {
     casa: {
         id: 'casa',
         sprite: "acarta/imagens/casa2.png",
         image: null,
-        floor: 440,
+        left: false,
+        right: false,
+        actors: [],
     },
-    casa2: {
-        id: 'casa2',
-        sprite: "acarta/imagens/casa.png",
-        image: null
-    }
+    quarto: {
+        id: 'quarto',
+        sprite: "acarta/imagens/quarto2.png",
+        image: null,
+        left: false,
+        right: false,
+        actors: [],
+    },
+    cave: {
+        id: 'cave',
+        sprite: "acarta/imagens/cave2.png",
+        image: null,
+        left: false,
+        right: false,
+        actors: [],
+    },
+    escola: {
+        id: 'escola',
+        sprite: "acarta/imagens/escola2.png",
+        image: null,
+        left: false,
+        right: false,
+        actors: [],
+    },
+    biblioteca: {
+        id: 'biblioteca',
+        sprite: 'acarta/imagens/biblioteca2.png',
+        image: null,
+        left: false,
+        right: false,
+        actors: [],
+    },
 }
 const actors = {
     mario: {
+        id: 'mario', 
         sprite: "/acarta/imagens/supermario.png",
         image: null,
-        x: 300,
-        newx: 300,
-        updatePos: () => {
-            if (actors.mario.newx > actors.mario.x) {
-                const diff = actors.mario.newx - actors.mario.x
-                if (diff > passos) {
-                    actors.mario.x = actors.mario.x + passos
+        x: 0,
+        y: 0,
+        sety: (y) => {
+            actors.mario.y = y - actors.mario.image.height
+        },
+        goto: false,
+        react: () => {
+            // verificar se temos de mover
+            if (actors.mario.goto) {
+                const distance = actors.mario.x - actors.mario.goto.x
+                if (Math.abs(distance) < step_distance) {
+                    // ator está a menos de um step do destino, mudar para lá
+                    actors.mario.x = actors.mario.goto.x
+                    actors.mario.goto = false
+                } else if (distance > 0) {
+                    // mover ator um step para a direita
+                    actors.mario.x -= step_distance
                 } else {
-                    actors.mario.x = actors.mario.x + diff
+                    // mover ator um step para a esquerda
+                    actors.mario.x += step_distance
                 }
-            } else {
-                const diff = actors.mario.x - actors.mario.newx
-                if (diff > passos) {
-                    actors.mario.x = actors.mario.x - passos
-                } else {
-                    actors.mario.x = actors.mario.x - diff
-                }
+            } 
+            // verificar se saimos do cenario
+            if (actors.mario.x < 40 && stage.scene.left) {
+                stage.scene = stage.scene.left
+                actors.mario.x = 600
+                actors.mario.goto = false
             }
+            if (actors.mario.x > 600 && stage.scene.right) {
+                stage.scene = stage.scene.right
+                actors.mario.x = 40
+                actors.mario.goto = false
+            } 
         }
-    }
+    },
+    barbara: {
+        id: 'barbara',
+        sprite: "/acarta/imagens/barbara2.png",
+        image: null,
+        x: 200,
+        y: 300,
+    },
+    boneca: {
+        id: 'boneca',
+        sprite: "/acarta/imagens/boneca2.png",
+        image: null,
+        x: 200,
+        y: 300,
+    },
+    felix: {
+        id: 'felix',
+        sprite: "/acarta/imagens/felix2.png",
+        image: null,
+        x: 200,
+        y: 300,
+    },
+    hugo: {
+        id: 'hugo',
+        sprite: "/acarta/imagens/hugo2.png",
+        image: null,
+        x: 200,
+        y: 300,
+    },
 }
 
 async function inicializar() {
     const bound = mycanvas.getBoundingClientRect() 
     // init scenes
-    scenes.casa2.image = new Image()
-    scenes.casa2.image.src = scenes.casa.sprite
-    await scenes.casa2.image.decode()
-    scenes.casa2.image = new Image()
-    scenes.casa2.image.src = scenes.casa2.sprite
-    await scenes.casa2.image.decode()
+    scenes.casa.image = new Image()
+    scenes.casa.image.src = scenes.casa.sprite
+    scenes.casa.left = scenes.escola
+    scenes.casa.right = scenes.quarto
+    scenes.casa.actors = [actors.barbara] 
+    await scenes.casa.image.decode()
+    loading.innerHTML = 'A carregar 10%'
+    progress.value = .1 
+    scenes.quarto.image = new Image()
+    scenes.quarto.image.src = scenes.quarto.sprite
+    scenes.quarto.left = scenes.casa
+    scenes.quarto.right = scenes.cave
+    scenes.quarto.actors = [actors.boneca] 
+    await scenes.quarto.image.decode()
+    loading.innerHTML = 'A carregar 20%'
+    progress.value = .2 
+    scenes.cave.image = new Image()
+    scenes.cave.image.src = scenes.cave.sprite
+    scenes.cave.left = scenes.quarto
+    scenes.cave.actors = [actors.felix] 
+    await scenes.cave.image.decode()
+    loading.innerHTML = 'A carregar 30%'
+    progress.value = .3 
+    scenes.escola.image = new Image()
+    scenes.escola.image.src = scenes.escola.sprite
+    scenes.escola.left = scenes.biblioteca
+    scenes.escola.right = scenes.casa
+    scenes.escola.actors = [actors.hugo] 
+    await scenes.escola.image.decode()
+    loading.innerHTML = 'A carregar 40%'
+    progress.value = .4 
+    scenes.biblioteca.image = new Image()
+    scenes.biblioteca.image.src = scenes.biblioteca.sprite
+    scenes.biblioteca.right = scenes.escola
+    scenes.biblioteca.actors = [actors.barbara] 
+    await scenes.biblioteca.image.decode()
+    loading.innerHTML = 'A carregar 50%'
+    progress.value = .5 
     // init actors
-    actors.supermario.image = new Image()
-    actors.supermario.image.src = actors.mario.sprite
-    await actors.supermario.image.decode()
-    // init stage
-    stage.scene = scenes.casa2
-    stage.actors.push(actors.supermario)
+    actors.mario.image = new Image()
+    actors.mario.image.src = actors.mario.sprite
+    await actors.mario.image.decode() 
+    loading.innerHTML = 'A carregar 60%'
+    progress.value = .6 
+    actors.mario.x = bound.width / 2 - actors.mario.image.width / 2  // initial position middle of scene
+    actors.mario.sety(floor)
+    actors.barbara.image = new Image()
+    actors.barbara.image.src = actors.barbara.sprite
+    await actors.barbara.image.decode() 
+    loading.innerHTML = 'A carregar 70%'
+    progress.value = .7 
+    actors.boneca.image = new Image()
+    actors.boneca.image.src = actors.boneca.sprite
+    await actors.boneca.image.decode() 
+    loading.innerHTML = 'A carregar 80%'
+    progress.value = .8 
+    actors.felix.image = new Image()
+    actors.felix.image.src = actors.felix.sprite
+    await actors.felix.image.decode() 
+    loading.innerHTML = 'A carregar 90%'
+    progress.value = .9 
+    actors.hugo.image = new Image()
+    actors.hugo.image.src = actors.hugo.sprite
+    await actors.hugo.image.decode() 
+    loading.innerHTML = 'A carregar 100%'
+    progress.value = 1
     
-    actors.supermario.scenePos = bound.width / 2 - actors.supermario.image.width / 2  // initial position middle of scene
+    // finish
+    stage.scene = scenes.casa
+    stage.actors.push(actors.mario)
+    renderStage() 
     loading.style.display = "none"
+    progress.style.display = "none"
     mycanvas.style.display = "block"
-}
-
-async function loadSceneByKey(key) {
-    const scene = scenes[Object.keys(scenes)[sceneKey]]
-    await loadScene(scene) 
-}
-async function loadScene(scene) {
-    ctx.drawImage(scene.image, 0, 0)
-}
-
-async function actorToScene(actor) {
-    actorTo(actor, actor.scenePos)
-}
-async function actorTo(actor, x) {
-    // update actor current position
-    actor.x = x
-    // update scene
-    const y = floor - actors.supermario.image.height
-    loadSceneByKey(sceneKey)
-    ctx.drawImage(actor.image, x, y)
-}
-
-async function actorMoveTo(actor, x) {
-    while (actor.x !== x) {
-        let newpos = -1 
-        if (actor.x > x) {
-            newpos = (actor.x - x) / 2 + x
-        } else {
-            newpos = actor, (x - actor.x) / 2 + actor.x
-        }
-        console.log(newpos)
-        await actorTo(actor, newpos)
-        await delay(500)
-    }
 }
 
 mycanvas.addEventListener("click", (ev) => {
     const bound = mycanvas.getBoundingClientRect() 
     const x = ev.clientX - bound.left
-    stage.actors[0].newx = x
+    actors.mario.goto = {
+        x: x,
+        y: actors.mario.y
+    }    
 })
 
 /** renderiza o palco */
 function renderStage() {
-    // render cenário
+    // console.log('rendering scene: ' + stage.scene.id)
     ctx.drawImage(stage.scene.image, 0, 0)
-    // atualiza atores
-    stage.actors.forEach( actor => {
-        actor.updatePos()
-    })
-    // render atores
-    stage.actors.forEach( actor => {
-        ctx.drawImage(actor.image, actor.x, stage.scene.floor - actor.image.height)
-    })
+    // render secondary actors for scene
+    stage.scene.actors.forEach( (actor) => {
+        // console.log('rendering actor ' + actor.id + ` at (${actor.x}, ${actor.y}) goto ` + (actor.goto ? `(${actor.goto.x}, ${actor.goto.y})` : 'false')) 
+        ctx.drawImage(actor.image, actor.x, actor.y)
+    }) 
+    // render main actor
+    ctx.drawImage(actors.mario.image, actors.mario.x, actors.mario.y)
 }
 
 function loop() {
-   console.log('loop: ' + Date.now()) 
-   // render stage
-   renderStage()
-   // update actors
-   setTimeout(loop, 40)
+    // console.log('loop: ' + Date.now())
+    // render stage
+    renderStage() 
+    // update actors
+    stage.actors.forEach( (actor) => {
+        actor.react()
+    })
+    setTimeout(loop, 50)
 }
 
 let sceneKey = 0
 inicializar().then( () => {
     console.log("Loading...")
+    // actorToScene(actors.mario)
     loop() 
 })
-
-
-
-function hitbox() {
-
-}
-
-
-
-function interacao1() {
-
-}
-
-function interacao2() {
-
-}
-
-function interacao3() {
-
-}
-
-function interacao4() {
-
-}
-
-function interacao5() {
-
-}
-
-function interacao6() {
-
-}
-
-function interacao7() {
-
-}
-
-function interacao8() {
-
-}
-
-function interacao9() {
-
-}
-
-function interacaolivro() {
-
-}
